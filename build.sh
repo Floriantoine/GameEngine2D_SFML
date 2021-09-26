9859d74970a70e9887b0766c9f8b67a88d099ce8
Floriantoine <florian.antoine@epitech.eu>
2021-09-26 01:18:23 -0400

    feat: push cmake v1

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+WORK_DIR="$(dirname "$(readlink -f "$0")")"
+BUILD_DIR="$WORK_DIR/build"
+CONAN_PROFILE="$WORK_DIR/conanprofile"
+
+mkdir -p "$BUILD_DIR" && \
+cd "$BUILD_DIR" && \
+conan install "$WORK_DIR" --build=missing --profile="$CONAN_PROFILE" && \
+cmake "$WORK_DIR" -G "Unix Makefiles" && \
+cmake --build "$BUILD_DIR" -j 8
