7f709627d4afa40bfaa9c5e104c1585f0714b3c9
agarof <alban.garofoli@epitech.eu>
2021-10-26 20:22:48 -0400

    Display cmake's progress when downloading dependencies

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,9 +1,12 @@
 include(FetchContent)
 
+set(FETCHCONTENT_QUIET OFF)
+
 FetchContent_Declare(
     json
     GIT_REPOSITORY https://github.com/nlohmann/json
     GIT_TAG v3.10.2
+    GIT_PROGRESS ON
     GIT_SHALLOW ON
 )
 
@@ -12,6 +15,7 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/SFML/sfml
     GIT_TAG 2.5.1
     GIT_SHALLOW ON
+    GIT_PROGRESS ON
 )
 
 FetchContent_MakeAvailable(
