0201a04a62b62f9cd37880425d1a913e1ccf4a54
Floriantoine <florian.antoine@epitech.eu>
2022-03-10 20:02:23 -0500

    fix: catch2 new version

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -24,7 +24,7 @@ FetchContent_Declare(
 FetchContent_Declare(
   Catch2
   GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-  GIT_TAG        v3.0.0-preview3
+  GIT_TAG        v3.0.0-preview4
 )
 
 FetchContent_MakeAvailable(
