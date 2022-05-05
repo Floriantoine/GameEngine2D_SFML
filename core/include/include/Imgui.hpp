1266df3635f2565dd9aa4c52b5771be178a61749
Floriantoine <florian.antoine@epitech.eu>
2022-05-05 14:34:57 -0400

    fix : clean duplicate include

--- core/include/include/Imgui.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "imgui-SFML.h"
-#include "imgui.h"
-#include "imgui/imfilebrowser.h"
-#include "imgui/implot.h"
-#include "imgui/implot_internal.h"
--- core/include/include/Tools.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "tools/Chrono.hpp"
-#include "tools/jsonTools.hpp"
-#include "tools/random.hpp"
-#include "tools/tools.hpp"
